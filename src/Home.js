b77302abe930022b57562b0cbc9e343e2eb76afe
imanda06 <imanda06@in.ibm.com>
2022-02-06 00:15:36 +0530

    Creating home page v1

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,74 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
-  <div>this is home Component</div>
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61rKFdQBcWL._SX3000_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product
+            title="Laptop"
+            price={"60000"}
+            image={
+              "https://m.media-amazon.com/images/I/71m03KItMZL._AC_SY200_.jpg"
+            }
+            rating={5}
+          />
+
+          <Product
+            title="Headphones"
+            price={"600"}
+            image={
+              "https://m.media-amazon.com/images/I/618M4jM0v1L._AC_UL480_FMwebp_QL65_.jpg"
+            }
+            rating={3}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="Watches"
+            price={"1000"}
+            image={
+              "https://m.media-amazon.com/images/I/61TY611i6LL._AC_UL480_FMwebp_QL65_.jpg"
+            }
+            rating={3}
+          />
+
+          <Product
+            title="Mobiles"
+            price={"10000"}
+            image={
+              "https://m.media-amazon.com/images/I/81S-XYJlzTL._AC_SY195_.jpg"
+            }
+            rating={1}
+          />
+          <Product
+            title="Chocolates"
+            price={"200"}
+            image={
+              "	https://m.media-amazon.com/images/I/510XEJhvuzS._SR215,215_.jpg"
+            }
+            rating={4}
+          />
+
+          <Product
+            title="Headphones"
+            price={"500"}
+            image={
+              "https://m.media-amazon.com/images/I/31tZ5WN2WXL._AC_SY200_.jpg"
+            }
+            rating={2}
+          />
+        </div>
+
+      </div>
+    </div>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Product.css";
+import StarRateIcon from "@mui/icons-material/StarRate";
+
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>₹</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>
+                <StarRateIcon />
+              </p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
